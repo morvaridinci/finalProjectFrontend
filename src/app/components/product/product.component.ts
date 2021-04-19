9d5c8bbbed2761b3fe3d5528c7566f1581fd76da
morvaridincikayik <morvaridincikayik@gmail.com>
2021-04-19 16:35:58 +0200

    day 19th

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,10 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
   filterText:"";
 
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) {}
+  constructor(private productService:ProductService,
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
    this.activatedRoute.params.subscribe(params=>{
@@ -38,4 +43,15 @@ export class ProductComponent implements OnInit {
       this.dataLoaded= true;
     });
   }
+
+  addToCart(product:Product){
+    if(product.productId===1){
+       this.toastrService.error("Bu urun sebete eklenemez","HATA")
+    }else{
+      this.toastrService.success("Sepete eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }
+    
+  }
+ 
 }
